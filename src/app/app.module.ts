72e69f9947a46cf9a6bcc11a441a1de6ba87633c
Jan Grzegorowski <grzegorowski@gmail.com>
2018-05-14 12:40:55 +0200

    feat: remove cordova native dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,6 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-
 @NgModule({
   declarations: [
     MyApp,
@@ -32,8 +29,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
